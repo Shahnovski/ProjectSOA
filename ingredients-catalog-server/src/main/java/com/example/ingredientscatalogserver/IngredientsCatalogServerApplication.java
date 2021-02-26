2655d249dd728058d9e90c2e902d5fde59aeed00
Shahnovski <shahnovski300899@gmail.com>
2021-02-26 08:56:42 +0300

    Initial commit

---
+++ ingredients-catalog-server/src/main/java/com/example/ingredientscatalogserver/IngredientsCatalogServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ingredientscatalogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientsCatalogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientsCatalogServerApplication.class, args);
+	}
+
+}
