fcf7c60efc7cfa102e16022052e506a743c08048
IgorAra <igor.arakelyan@list.ru>
2021-02-27 20:36:13 +0300

    Init commit

---
+++ internet-shop-server/src/test/java/com/example/internetshopserver/InternetShopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internetshopserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
