8d4eb33db3ec495618a499a8667404b28313e311
Shahnovski <shahnovski300899@gmail.com>
2021-02-28 16:22:11 +0300

    Added ingredient class and service

---
+++ ingredients-catalog-client/src/app/ingredient.ts
@@ -0,0 +1,10 @@
+
+export class Ingredient {
+  id: number = 0;
+  ingredientCode: number = 0;
+  ingredientName: string = '';
+  ingredientCalories: number = 0;
+  ingredientProteins: number = 0;
+  ingredientCarbohydrates: number = 0;
+  ingredientFats: number = 0;
+}
