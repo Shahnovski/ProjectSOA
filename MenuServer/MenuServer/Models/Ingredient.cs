e54863bbe74463e2f67a0b15d5f59a1eba09774d
Shahnovski <shahnovski300899@gmail.com>
2021-03-07 23:51:14 +0300

    Init MenuServer

---
+++ MenuServer/MenuServer/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuServer.Models
+{
+    public class Ingredient
+    {
+        [Key]
+        public int IngredientId { get; set; }
+    }
+}
